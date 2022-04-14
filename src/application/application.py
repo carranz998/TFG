ba3515d10258ee8ce477db3072b78b66d220cfd9
Carlos Arranz Sánchez <carranz998@gmail.com>
2022-04-14 14:48:19 +0200

    Implement template ProGAN

--- src/application/application.py
+++ src/application/application.py
@@ -1,6 +1,8 @@
+from torch.utils.data import DataLoader
+import torchvision.datasets
 from data_preprocessing.input_image_preprocessor import InputImagePreprocessor
 from utils.command_line_parser import CommandLineParser
-from utils.filepath_management import FilepathManagement
+from utils.directory_iterator import DirectoryIterator
 
 
 class Application:
@@ -9,10 +11,16 @@ class Application:
         self.command_line_parsed_args = command_line_parser.parsed_args
 
     def execute(self) -> None:
-        abspath_directory_images = self.command_line_parsed_args.abspath_directory_images
-        
-        input_image_preprocessor = InputImagePreprocessor()
-        abspath_subfiles = FilepathManagement.get_abspath_subfiles(abspath_directory_images)
+        abspath_directory_images = self.command_line_parsed_args.abspath_directory_images    
+
+        dataset = torchvision.datasets.ImageFolder(root=abspath_directory_images)
+        batch_size = 2
+        data_loader = DataLoader(dataset, batch_size=batch_size)
+
+        abspath_subfiles = DirectoryIterator.subtree(abspath_directory_images)
+
+        image_axis_deleter = InputImagePreprocessor()
 
         for abspath_subfile in abspath_subfiles:
-            input_image_preprocessor.delete_axis(abspath_subfile)
+            image_axis_deleter.delete_axis(abspath_subfile)
+            break
---
+++ src/utils/directory_iterator.py
@@ -0,0 +1,14 @@
+import os
+from typing import Iterator
+
+
+class DirectoryIterator:
+    @classmethod
+    def subtree(cls, abspath_root_folder: os.path.abspath) -> Iterator[os.path.abspath]:
+        contents_root_folder = os.walk(abspath_root_folder, topdown=False)
+
+        for abspath_subfolder, _, basenames_subfolder in contents_root_folder:
+            for basename in basenames_subfolder:
+                abspath_subfile = os.path.join(abspath_subfolder, basename)
+
+                yield abspath_subfile
